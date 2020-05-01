0af9a647bea264d044ce7ae4a89fd47cd20870da
mehdirahbe <mehdirahbvd@gmail.com>
2020-05-01 21:07:48 +0200

    Add TeslaCarInfo model to admin page

--- matesla/admin.py
+++ matesla/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import TeslaToken, TeslaFirmwareHistory
+from .models import TeslaToken, TeslaFirmwareHistory, TeslaCarInfo
 
 admin.site.register(TeslaToken)
 admin.site.register(TeslaFirmwareHistory)
+admin.site.register(TeslaCarInfo)
