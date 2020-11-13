a02798e3383c7572b7640a9995b1ae59b8b86101
mehdirahbe <mehdirahbvd@gmail.com>
2020-11-14 00:41:46 +0100

    Translate all that pw reset thing in french

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,11 +1,11 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-
+from django.utils.translation import ugettext_lazy as _
 
 class SignUpForm(UserCreationForm):
-    email = forms.EmailField(max_length=254, help_text='Required to be able to reset password.')
+    email = forms.EmailField(max_length=254, help_text=_('Required to be able to reset password if you forget it'))
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2', )
+        fields = ('username', 'email', 'password1', 'password2')
