be0c9733c962e6379b422b94af0eef6906b32ded
mehdirahbe <mehdirahbvd@gmail.com>
2020-11-14 00:00:38 +0100

    When creating a user, save its email, so that pw reset is possible

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
 # Create your views here.
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
 
+from accounts.forms import SignUpForm
+
 
 class SignUp(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = SignUpForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
